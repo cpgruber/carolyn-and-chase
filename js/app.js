83c05439a7cb4fd900299e179b8e14d48bb31f9a
Chase Gruber <chasegruber@gmail.com>
2017-07-13 21:04:46 -0400

    basically done

--- js/app.js
+++ js/app.js
@@ -4,10 +4,32 @@
     "pouchdb"
   ])
   .run(Run)
+  .directive("scrollTo", scrollTo)
 
   Run.$inject = ["dbs"];
   function Run(dbs){
     dbs.sync();
+  }
+
+  function scrollTo(){
+    var directive = {
+      link: link,
+      restrict: "A"
+    };
+    return directive;
+
+    function link(scope, el, attrs){
+      el.on("click", function(){
+        try {
+          var target = angular.element(document.querySelector(attrs.href));
+          var val = target.offset().top;
+        }catch(err){
+          var val = 0;
+        }
+        angular.element(document.body).animate({scrollTop: val}, "slow");
+      });
+    }
+
   }
 
 }())
